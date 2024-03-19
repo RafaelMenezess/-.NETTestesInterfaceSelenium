7e8f3b56ba362d0370f8a92b5422a276f6d3f3bc
Rafael Menezes <rafa.rcm@hotmail.com>
2024-03-19 11:08:54 -0300

    Usando localizadores e cadastrando um cliente

--- Alura.ByteBank.WebApp.Testes/AposRealizarLogin.cs
+++ Alura.ByteBank.WebApp.Testes/AposRealizarLogin.cs
@@ -80,5 +80,41 @@ namespace Alura.ByteBank.WebApp.Testes
 
             driver.Close();
         }
+
+        [Fact]
+        public void RealizarLoginAcessaMenuECadastraCliente()
+        {
+            //Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
+
+            var login = driver.FindElement(By.Name("Email"));
+            var senha = driver.FindElement(By.Name("Senha"));
+
+            login.SendKeys("rafael@email.com");
+            senha.SendKeys("senha01");
+
+            driver.FindElement(By.Id("btn-logar")).Click();
+
+            driver.FindElement(By.LinkText("Cliente")).Click();
+
+            driver.FindElement(By.LinkText("Adicionar Cliente")).Click();
+
+            driver.FindElement(By.Name("Identificador")).Click();
+            driver.FindElement(By.Name("Identificador")).SendKeys("2df71922-ca7d-4d43-b142-0767b32f822a");
+            driver.FindElement(By.Name("CPF")).Click();
+            driver.FindElement(By.Name("CPF")).SendKeys("69981034096");
+            driver.FindElement(By.Name("Nome")).Click();
+            driver.FindElement(By.Name("Nome")).SendKeys("Tobey Garfield");
+            driver.FindElement(By.Name("Profissao")).Click();
+            driver.FindElement(By.Name("Profissao")).SendKeys("Cientista");
+
+            //Act
+            driver.FindElement(By.CssSelector(".btn-primary")).Click();
+            driver.FindElement(By.LinkText("Home")).Click();
+
+            //Assert 
+            Assert.Contains("Logout", driver.PageSource);
+        }
     }
 }
